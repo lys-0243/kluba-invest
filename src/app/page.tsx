370599793b2b5bead87e6bd2786df21a489573ef
lys-0243 <sylvain.tshiasuma2017@gmail.com>
2024-02-14 19:37:01 +0100

    config dev depencies2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-red-600 font-U-medium text-9xl">
-      Page d'accueil
+      Page daccueil
     </main>
   );
 }
