fa9c45408b47ac817e5bf808fde6dcdc6b79234d
lys-0243 <sylvain.tshiasuma2017@gmail.com>
2024-02-15 11:03:06 +0100

    home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,48 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-red-600 font-U-medium text-9xl">
-      Page daccueil
+    <main className="bg-orange ">
+      <section className="bg-[url('/img/home/bg-home.png')] bg-cover min-h-screen">
+        <nav className="mx-auto 2xl:max-w-screen-xl xl:max-w-screen-xl flex justify-between items-center">
+          <div>
+            <Link href="/">
+              <Image
+                src="/img/logos/kaapital-logo-blanc.png"
+                alt="Logo Kluba"
+                width={110}
+                height={100}
+              />
+            </Link>
+          </div>
+
+          <div className=" text-text font-U-medium  space-x-8">
+            <Link href="#" key="#" className="">
+              accueil
+            </Link>
+            <Link href="#" key="#" className="">
+              investir/épargner
+            </Link>
+            <Link href="#" key="#" className="">
+              blog
+            </Link>
+          </div>
+
+          <div>
+            <Link href="#">S&apos;inscrire</Link>
+          </div>
+        </nav>
+      </section>
+
+      <section>
+        autre section <br />
+        autre section <br />
+        autre section <br />
+        autre section <br />
+      </section>
     </main>
   );
 }
+
+// mx-auto 2xl:max-w-screen-xl xl:max-w-screen-xl
